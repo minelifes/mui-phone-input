0796c7629919774afc93ad272cbc2bafc4892b4f
Arseny Garelyshev <arseny@lifekeys.no>
2023-02-20 14:20:14 +0100

    fix: don't crash if doubleClick event target lacks value
    Refs: #17

--- src/index.js
+++ src/index.js
@@ -638,8 +638,10 @@ class PhoneInput extends React.Component {
   };
 
   handleDoubleClick = (e) => {
-    const len = e.target.value.length;
-    e.target.setSelectionRange(0, len);
+    if (e.target.value) {
+      const len = e.target.value.length;
+      e.target.setSelectionRange(0, len);
+    }
   };
 
   handleFlagItemClick = (country, e) => {
