dc8e0155b01fe810d526cbc00a48cc23aedf91b4
Harish Sreeramappa <harish.s@mable.com.au>
2025-06-23 11:56:08 +0530

    Upgrade webpack packages

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Highly customizable phone input component with auto formatting. Based on the wonderful [react-phone-input-2](https://github.com/bl00mber/react-phone-input-2) package.
 
-Supports material-ui v5.
+Supports Material-UI v5 and v6.
 
 ### Original look:
 
--- test/dev_js/demo.js
+++ test/dev_js/demo.js
@@ -63,15 +63,6 @@ class Demo extends React.Component {
         `,
           }}
         />
-        <p style={{ fontWeight: "500" }}>
-          Created by{" "}
-          <a
-            style={{ color: "#000" }}
-            href="https://github.com/bl00mber/react-phone-input-2"
-          >
-            Nick Reiley
-          </a>
-        </p>
         <div style={{ display: "inline-block", verticalAlign: "top" }}>
           <p>Exclude countries (usa, canada)</p>
           <PhoneInput
